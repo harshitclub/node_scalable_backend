5dd5c2d99ce86565ec0a31f89ce1fd7047a340ef
harshitclub <harshitclub@gmail.com>
2025-10-03 16:14:59 +0530

    @removing sensitive information

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,27 +8,6 @@ module.exports = {
       watch: false,
       env_production: {
         NODE_ENV: "production",
-        PORT: 5000,
-
-        DATABASE_URL:
-          "postgresql://harshit:Harshit@7505@localhost:5432/mydb?schema=public",
-
-        REDIS_CACHE_URL: "redis://localhost:6380",
-        REDIS_BULL_URL: "redis://localhost:6381",
-
-        EMAIL_WORKER_CONCURRENCY: 5,
-
-        JWT_VERIFICATION_SECRET: "0RJh1mRucS3Ae3zrXILTSzJ8IuGUDE6L",
-        JWT_ACCESS_SECRET: "3EloBXLfvXObL7hvmKKJhys1P9kHJeAk",
-        JWT_REFRESH_SECRET: "2MovPw2wmrNn3FdeqIOgwXeIljjobWtW",
-
-        SMTP_HOST: "smtp.hostinger.com",
-        SMTP_PORT: 465,
-        SMTP_USER: "harshit@cryptoxora.com",
-        SMTP_PASS: "Harshit@7505",
-        MAIL_FROM: "SLB Backend <harshit@cryptoxora.com>",
-
-        FRONTEND_URL: "http://localhost:3000",
       },
     },
     {
